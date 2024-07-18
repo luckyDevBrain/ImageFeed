846e25bd80da82b0d2242f5890d75ef1ea094035
luckyDevBrain <luckyDevBrain@yandex.ru>
2024-07-18 18:25:56 +0300

    Initial commit for ImageFeed project

---
+++ ImageFeed/ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Kirill on 18.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
