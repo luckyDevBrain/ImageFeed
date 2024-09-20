d49f046c3c34a983de8cccfc6586ecdcf51d5ebf
luckyDevBrain <luckyDevBrain@yandex.ru>
2024-09-20 21:58:02 +0300

    усердно тружусь над 11 спринтом

---
+++ ImageFeed/ImageFeed/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  ImageFeed
+//
+//  Created by Kirill on 19.09.2024.
+//
+
+import Foundation
--- ImageFeed/ImageFeed/ServiceAccessLayer/Network/NetworkClient.swift
+++ ImageFeed/ImageFeed/ServiceAccessLayer/Network/NetworkClient.swift
@@ -2,45 +2,7 @@
 //  NetworkClient.swift
 //  ImageFeed
 //
-//  Created by Kirill on 25.08.2024.
+//  Created by Kirill on 20.09.2024.
 //
 
 import Foundation
-
-protocol NetworkRouting {
-    func fetch(request: URLRequest, handler: @escaping (Result<Data, Error>) -> Void)
-}
-
-struct NetworkClient: NetworkRouting {
-    
-    private enum NetworkError: Error {
-        case codeError
-    }
-    
-    func fetch(request: URLRequest, handler: @escaping (Result<Data, Error>) -> Void) {
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                print("Error in network request: \(error.localizedDescription)")
-                handler(.failure(error))
-                return
-            }
-            
-            if let response = response as? HTTPURLResponse,
-               response.statusCode < 200 || response.statusCode >= 300 {
-                print("HTTP error with status code: \(response.statusCode)")
-                handler(.failure(NetworkError.codeError))
-                return
-            }
-            
-            guard let data = data else {
-                print("No data received from the server")
-                return
-            }
-            handler(.success(data))
-        }
-        
-        task.resume()
-    }
-}
