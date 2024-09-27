f8acd48d371bcda755ffb71a35add328de095491
luckyDevBrain <luckyDevBrain@yandex.ru>
2024-09-27 21:57:11 +0300

    Создал файлы: AuthConfiguration, AuthHelper, WebViewPresenter

---
+++ ImageFeed/ImageFeed/AuthConfiguration.swift
@@ -0,0 +1,42 @@
+//
+//  AuthConfiguration.swift
+//  ImageFeed
+//
+//  Created by Kirill on 11.08.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "nVfXTJMJtzNl0VD5sbctnSDXz3yg8Imzj205X4lxKcw"
+    static let secretKey = "h7imQS1BJ6AI8kzYSW_ZRouqCfMA7Zpty855DjppoSc"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultPhotos = "https://api.unsplash.com/photos/"
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+
+    static var standard: AuthConfiguration {
+            return AuthConfiguration(accessKey: Constants.accessKey,
+                                     secretKey: Constants.secretKey,
+                                     redirectURI: Constants.redirectURI,
+                                     accessScope: Constants.accessScope,
+                                     defaultBaseURL: (Constants.defaultBaseURL),
+                                     authURLString: Constants.unsplashAuthorizeURLString)
+        }
+}
+
+enum HTTPMethods {
+    static let get = "GET"
+    static let post = "POST"
+    static let delete = "DELETE"
+}
